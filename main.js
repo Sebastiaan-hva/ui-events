b6892e2025a74bf5d8458dcc7f993142eccc8f47
Sebastiaan <sebastiaan.hagoort@hva.nl>
2024-12-09 12:59:26 +0100

    wic

--- main.js
+++ main.js
@@ -4,22 +4,20 @@
 */
 
 // Stap 1: selecteer het 12e linkje, en sla deze op in een variabele
-let interaction = document.querySelector('a:nth-of-type(12)')
+
 
 // Stap 2: voeg de (click) event listener toe aan de link, met een callback functie
 // In dit geval wordt de jumpHandler functie aangeroepen, zodra je op het linkje klikt
-interaction.addEventListener('click', jumpHandler)
+
 
 // Deze jumpHandler functie staat klaar voor als we 'm aanroepen; deze wordt dus
 // _niet_ meteen bij het laden van de pagina aangeroepen
-function jumpHandler() {
+
 
   // Stap 3: toggle een class op het element (zodra er dus op een link wordt geklikt)
-  interaction.classList.toggle('jump')
-}
+
 
 // Extraatje, waardoor de class weer weggehaald wordt zodra de animatie afgelopen is
-interaction.addEventListener('animationend', jumpHandler)
 
 
 // Ga zelf verder met de overige elementen, aan de hand van de instructies
@@ -40,6 +38,7 @@ dblClick.addEventListener('dblclick', (e) => {
 
 // Stap 2: addEventListener
 // bibberLink.addEventListener...
+
 let spinToWin = document.querySelector('a:nth-of-type(3)')
 spinToWin.addEventListener('keydown', onkeydown);
 onkeydown = (event) => {
@@ -61,3 +60,23 @@ function flickerHandler () {
   flickerButton.classList.toggle('flicker');
 }
 flickerButton.addEventListener('animationend', flickerHandler)
+
+
+
+// let expandButton = document.querySelector('a:nth-of-type(7)')
+
+// expandButton.addEventListener('click', expandHandler)
+
+// function expandHandler () {
+//   expandButton.classList.toggle('expand');
+// }
+// expandButton.addEventListener('animationend', expandHandler)
+
+let interaction = document.querySelector('a:nth-of-type(12)') 
+  interaction.addEventListener('click', jumpHandler)  
+
+function jumpHandler() {  
+  interaction.classList.toggle('jump');
+}
+
+interaction.addEventListener('animationend', jumpHandler)
